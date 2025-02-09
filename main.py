8ac13206c7f30825287e5736edb627b41a1c49be
Karsten Jochens <karsten.jochens@gmail.com>
2025-02-09 22:39:53 +0100

    Entferne die MQTT-Client-Implementierung und aktualisiere die Abhängigkeiten; verbessere die Geräte-Statusüberprüfung in der XSenseAPI

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
 import time
-from mqtt_client import MQTTClient
-from xsense_api import XSenseAPI
+from xsense_api import XSenseApi
+from mqtt import MqttClient
+from config import MQTT_BROKER, MQTT_PORT, MQTT_TOPIC
 
-def monitor_and_send_status(xsense_api, mqtt_client):
-    """Überwacht Geräte und sendet Statusänderungen per MQTT"""
-    while True:
-        for house in xsense_api.api.houses.values():
-            for station in house.stations.values():
-                status_info = xsense_api.get_device_status(station)
-                
-                # Wenn Statusänderungen vorliegen, an MQTT senden
-                for status in status_info:
-                    mqtt_client.send_message(status)
+def fetch_and_publish_data():
+    xsense_api = XSenseApi()
+    xsense_api.initialize()
+    
+    states = xsense_api.fetch_data()
 
-        # Alle 10 Sekunden nach Änderungen suchen (anpassbar)
-        time.sleep(10)
+    mqtt_client = MqttClient(MQTT_BROKER, MQTT_PORT)
+    mqtt_client.publish(states)
 
 def main():
-    # MQTT-Client initialisieren
-    mqtt_client = MQTTClient()
-    
-    # XSense API initialisieren und anmelden
-    xsense_api = XSenseAPI()
-    xsense_api.login()
-    
-    # Starten der Überwachung und des Sendens
-    monitor_and_send_status(xsense_api, mqtt_client)
+    while True:
+        fetch_and_publish_data()
+        time.sleep(30)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- xsense_api.py
+++ xsense_api.py
@@ -1,32 +1,20 @@
 from xsense import XSense
-import config  # Importiere die Konfiguration
+from config import XSENSE_USERNAME, XSENSE_PASSWORD
 
-class XSenseAPI:
+class XSenseApi:
     def __init__(self):
         self.api = XSense()
-        self.username = config.XSENSE_USERNAME
-        self.password = config.XSENSE_PASSWORD
-        self.previous_states = {}
 
-    def login(self):
-        """Login bei der XSense API"""
+    def initialize(self):
         self.api.init()
-        self.api.login(self.username, self.password)
-        self.api.load_all()
+        self.api.login(XSENSE_USERNAME, XSENSE_PASSWORD)
 
-    def get_device_status(self, station):
-        """Überprüfe den Status der Station und der Geräte"""
-        status_info = []
-        
-        station_state = self.api.get_station_state(station)
-        if station_state != self.previous_states.get(station.id):
-            status_info.append(f"Station: {station.name}, Status: {station_state}")
-            self.previous_states[station.id] = station_state
-        
-        for device in station.devices.values():
-            device_state = self.api.get_state(device)
-            if device_state != self.previous_states.get(device.id):
-                status_info.append(f"Device: {device.name}, Status: {device_state}")
-                self.previous_states[device.id] = device_state
-        
-        return status_info
+    def fetch_data(self):
+        device_data = {}
+        self.api.load_all()
+        for _, house in self.api.houses.items():
+            for _, station in house.stations.items():
+                state = self.api.get_state(station)
+                if state:
+                    device_data[station.serial] = state.values if state.values else None
+        return device_data
